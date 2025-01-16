8889d25d08179bd2b2ef4b80cdddcfb0a13d9cad
Geremia Taglialatela <tagliala.dev@gmail.com>
2025-01-16 18:56:25 +0100

    Unsafe offenses

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,17 +8,16 @@ require 'filemagic/ext'
 
 SimpleCov.start
 
-SPEC_BASE = Pathname.new(__FILE__).realpath.parent
+SPEC_BASE = Pathname.new(__dir__)
 
-$: << (SPEC_BASE.parent + 'lib')
 require 'colore-client'
 
 def fixture(name)
-  SPEC_BASE + 'fixtures' + name
+  SPEC_BASE.join('fixtures', name)
 end
 
 VCR.configure do |c|
-  c.cassette_library_dir = (SPEC_BASE + 'fixtures' + 'cassettes').to_s
+  c.cassette_library_dir = SPEC_BASE.join('fixtures/cassettes')
   c.hook_into :webmock
   c.configure_rspec_metadata!
 end
