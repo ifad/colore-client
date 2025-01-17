6653b075695968e5e3b591f63df54fcb72ff7121
Geremia Taglialatela <tagliala@users.noreply.github.com>
2025-01-17 08:56:35 +0100

    Fix unsafe offenses in production code (#11)

--- lib/colore-client.rb
+++ lib/colore-client.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 require_relative 'colore/errors'
 require_relative 'colore/client'
--- lib/colore/errors.rb
+++ lib/colore/errors.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Colore
   module Errors
     class ColoreUnavailable < StandardError
